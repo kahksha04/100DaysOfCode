190ea85719e5b0c1b12c86ba0d90803987c80af4
kahksha04 <132389460+kahksha04@users.noreply.github.com>
2023-06-28 22:45:07 +0530

    day25.cpp

---
+++ day25.cpp
@@ -0,0 +1,56 @@
+// Given a preorder traversal of a BST, find the leaf nodes of the tree without building the tree.
+
+// User function Template for C++
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+class Solution {
+  public:
+    Node* Creation(Node* root,int val){
+        if(root == NULL){
+            root = new Node(val);
+            return root;
+        }
+        
+        if(root->data > val){
+            root->left = Creation(root->left,val);
+            return root;
+        }else{
+            root->right = Creation(root->right,val);
+            return root;
+        }
+    }
+    void LeafNode(Node* root,vector<int>& arr){
+        if(root == NULL){
+            return;
+        }
+        
+        if(root->left == NULL && root->right == NULL){
+            arr.push_back(root->data);
+        }
+        
+        LeafNode(root->left,arr); 
+        LeafNode(root->right,arr); 
+    }
+  public:
+    vector<int> leafNodes(int arr[],int N) {
+        Node* root = NULL;
+        
+        for(int i = 0 ; i < N ; i++){
+            root = Creation(root,arr[i]);
+        }
+        
+        vector<int> ans;
+        LeafNode(root,ans);
+        
+        return ans;
+    }
+};
