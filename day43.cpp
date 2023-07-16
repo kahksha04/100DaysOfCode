7631845fbfc524d31481ffbc604e240681248451
kahksha04 <132389460+kahksha04@users.noreply.github.com>
2023-07-16 20:26:42 +0530

    day43.cpp

---
+++ day43.cpp
@@ -0,0 +1,25 @@
+// Implement a Queue using Linked List. 
+
+void MyQueue:: push(int x)
+{
+        // Your Code
+        QueueNode *temp=new QueueNode(x);
+        if(front==NULL){
+            front=rear=temp;
+        }else{
+        rear->next=temp;
+        rear=temp;
+        }
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code  
+        if(front==NULL){
+            return -1;
+        }
+        int val=front->data;
+        front=front->next;
+        return val;
+}
