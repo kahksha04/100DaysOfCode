677cdd202bc52ea72c6f3cae37c3c05387e7a861
kahksha04 <132389460+kahksha04@users.noreply.github.com>
2023-07-21 22:41:56 +0530

    day48.cpp

---
+++ day48.cpp
@@ -0,0 +1,32 @@
+//Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a
+//multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed 
+
+class Solution
+{
+    public:
+    
+    
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        
+        if(head==NULL){
+            return NULL;
+        }
+        node *temp=head;
+        node *prev=NULL;
+        node *curr=NULL;
+        int c=0;
+        while(temp!=NULL && c<k){
+            curr=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=curr;
+            c++;
+        }
+        if(curr!=NULL){
+            head->next=reverse(curr,k);
+        }
+        return prev;
+    }
+};
